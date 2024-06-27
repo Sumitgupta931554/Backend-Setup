3b8d5da6d3794f149989818d6db1e4a22d327379
Sumit Gupta <sumitgupta931554@gmail.com>
2024-06-27 21:15:21 +0530

    creating controller and router for testing and debugging

--- src/app.js
+++ src/app.js
@@ -11,11 +11,15 @@ app.use(cors({
 app.use(express.json({
     limit: "16kb"
 }))
-app.use(express.unlencode({
+app.use(express.urlencoded({
     extended:true,
     limit:"16kb"
 }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//router import
+import userRouter from "./routes/user.routes.js"
+app.use("/api/v1/user",userRouter)
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import DB_Connect from "./db/index.js";
+import app from "./app.js"
 import dotenv from "dotenv";
 dotenv.config();
 DB_Connect()
 .then(()=>{
     app.listen(process.send.PORT||8000,()=>{
-        console.log(`Server is runnnng at Port : ${process.env.POrt}`);
+        console.log(`Server is runnnng at Port : ${process.env.PORT}`);
     })
 })
 .catch((err)=>{
