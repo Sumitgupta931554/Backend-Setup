0c6f75900b3e8175f4200410527c7f452dd9db6f
Sumit Gupta <sumitgupta931554@gmail.com>
2024-06-29 11:39:01 +0530

    Working On registering the user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,12 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"Successfully Done"
-    })
+    // res.status(200).json({
+    //     message:"Successfully Done"
+    // })
+
+    const {fullname,email,username,password}=req.body;
+    console.log("Fullname",fullname);
 })
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router= Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields(),
+    registerUser)
 
 export default router;
